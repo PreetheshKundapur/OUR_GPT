8298c0f74d1a33fedd5dd6f5842ab8bf7ed5d83c
Adithya_G <adithyaacharya710@gmail.com>
2025-10-04 13:27:22 +0530

    Changed Hero section

--- frontend/src/components/main/Hero.jsx
+++ frontend/src/components/main/Hero.jsx
@@ -5,8 +5,8 @@ export default function Hero() {
   const user = useSelector((state) => state.user?.name || "Guest");
   return (
     <div className="h-10/12 px-4 flex flex-col items-center justify-center">
-      <p className="mb-4 text-center text-md lg:text-xl font-bold text-gray-300">Hello {user}</p>
-      <h1 className='text-center text-5xl lg:text-5xl font-bold text-gray-300'>Conversations Reimagined. Intelligence at Your Fingertips.</h1>
+      <p className="mb-4 text-center text-3xl lg:text-4xl font-medium text-indigo-400">Hello, {user}</p>
+      <h1 className='text-center text-3xl lg:text-5xl font-bold text-gray-300'>What can I help with?</h1>
     </div>
   )
 }
