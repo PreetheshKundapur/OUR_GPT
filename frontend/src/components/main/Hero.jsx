60e8d7d181f37daf0072b3e2d813792ab06daa0d
Adithya_G <adithyaacharya710@gmail.com>
2025-10-04 13:17:04 +0530

    Added user name in the Hero

--- frontend/src/components/main/Hero.jsx
+++ frontend/src/components/main/Hero.jsx
@@ -1,8 +1,12 @@
+import { use } from "react";
+import { useSelector } from "react-redux";
+
 export default function Hero() {
+  const user = useSelector((state) => state.user?.name || "Guest");
   return (
     <div className="h-10/12 px-4 flex flex-col items-center justify-center">
-      <p className="text-center text-md lg:text-xl font-bold text-gray-300">Hello I'm Preetesh</p>
-      <h1 className='text-center text-5xl lg:text-6xl font-bold text-gray-300'>Conversations Reimagined. Intelligence at Your Fingertips.</h1>
+      <p className="mb-4 text-center text-md lg:text-xl font-bold text-gray-300">Hello {user}</p>
+      <h1 className='text-center text-5xl lg:text-5xl font-bold text-gray-300'>Conversations Reimagined. Intelligence at Your Fingertips.</h1>
     </div>
   )
 }
