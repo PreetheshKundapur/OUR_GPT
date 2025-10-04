048e93f4edee43de28ec804320fb89157659ca8f
PreetheshKundapur <preetheshpk51@gmail.com>
2025-10-04 15:03:18 +0530

    Hero section changes (2)

--- frontend/src/components/main/Hero.jsx
+++ frontend/src/components/main/Hero.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 
 export default function Hero() {
   const user = useSelector((state) => state.user?.name || "Guest");
-  const user = useSelector((state) => state.user?.name || "Guest");
+
   return (
     <div className="h-10/12 px-4 flex flex-col items-center justify-center">
       <p className="mb-4 text-center text-3xl lg:text-4xl font-medium text-indigo-400">Hello, {user}</p>
